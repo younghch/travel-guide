9fd168875717c0eaacde81c5b80b4b2bacbc0338
Young <younghch42@gmail.com>
2023-12-03 21:20:14 +0900

    refactor: improve prompt

--- core/gpt.py
+++ core/gpt.py
@@ -11,18 +11,24 @@ You are a hisotrian. Your role is to write a brief audio guide of the nearby pla
 You will be given a list of famous places in the area.
 
 Requirements:
-- Include historical facts about the place.
-- Include interesting stories about the place.
-- If the places are related to each other, explain the relationship.
+- Descirbe the cultural, historical background about the place.
+- If places are related to each other, make a story with related places, describe at once.
+- Tell interesting stories behind the place.
 - Describe what the tourists can do in the place.
-- Exclude the places that are not historically important.
+- Exclude the places that are not important.
 
 Always answer in Korean.
 """
 
-SYSTEM_PROMPT_DETAIL= """
-You are a historian. You will be provided a place user want to be guided. 
-Your role is to write audio tour guide of the place.
+SYSTEM_PROMPT_RIGHT_INFRONT= """
+You are a historian working as a tour guide. 
+You will be provided a cultural heritage or place where you are standing infront of. 
+Your role is to guide the place in detail to foreign tourist.
+
+Requirements:
+- Descirbe the cultural, historical background about the places.
+- Describe how the place looks, characteristic and meanings of each part.
+- Tell interesting stories behind the places.
 
 Always answer in Korean.
 """
@@ -31,7 +37,7 @@ client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY"),
 )
 
-def get_general_guide_of_places(places: list[str], model=3):
+def get_general_guide_of_places(places: list[str], gpt_version=3):
     chat_completion = client.chat.completions.create(
         messages=[
             {
@@ -43,25 +49,25 @@ def get_general_guide_of_places(places: list[str], model=3):
                 "content": ', '.join(places),
             }
         ],
-        model=GPT_MODEL.get(model),
+        model=GPT_MODEL.get(gpt_version),
     )
     print(chat_completion.choices[0].message)
     return chat_completion.choices[0].message.content
 
-def get_detailed_guide_of_a_place(place: str, model=3):
+def get_detailed_guide_of_places_right_infront(places: list[str], gpt_version=3):
     print("running detailed guide")
     chat_completion = client.chat.completions.create(
         messages=[
             {
                 "role": "system",
-                "content": SYSTEM_PROMPT_DETAIL,
+                "content": SYSTEM_PROMPT_RIGHT_INFRONT,
             },
             {
                 "role": "user",
-                "content": place,
+                "content": ', '.join(places),
             }
         ],
-        model=GPT_MODEL.get(model),
+        model=GPT_MODEL.get(gpt_version),
     )
     print(chat_completion.choices[0].message)
     return chat_completion.choices[0].message.content
